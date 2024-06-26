bab62c79124f59d77411fb5c67e5fee46fb57a49
Kanchanprajapat <141110309+Kanchanprajapat@users.noreply.github.com>
2024-06-26 12:26:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstWord(s) {
   // your code here
+	if(s.length == 0){
+		return '';
+	}
+	var words = s.split(' ');
+    
+    // Check if the first word is empty
+    if (words[0] == '') {
+        // If it is, return the second word
+        return words[1];
+    } else {
+        // Otherwise, return the first word
+        return words[0];
+    }
 }
 
 // Do not change the code below
